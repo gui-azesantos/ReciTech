2cc2be423d7c6fba670032b250451b3c7671f39c
Guilherme Azevedo <gui-azesantos@hotmail.com>
2024-06-17 14:36:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ReciTech
+Aplicação ReciTech baseada na Next Level Week #1 da Rocketseat, feita com HTML e CSS puro.
---
+++ src/server.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const server = express();
+
+// get the database
+const db = require("./database/db");
+
+// configure public folder
+server.use(express.static("public"));
+
+// able the use of req.body
+server.use(express.urlencoded({ extended: true }));
+
+// use template engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views/", {
+    express: server,
+    noCache: true,
+});
+
+// # routes configures of the application #
+// homepage
+// req: resquisition
+// res: response
+server.get("/", (req, res) => {
+    return res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+    // query strings
+    // console.log(req.query)
+
+    return res.render("create-point.html");
+});
+
+server.post("/savepoint", (req, res) => {
+    // req.body
+    // console.log(req.body)
+
+    // insert data on the database
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `;
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items,
+    ];
+
+    db.run(query, values, function (err) {
+        if (err) {
+            console.log(err);
+            return res.send("Erro no cadastro");
+        }
+
+        console.log("[personal] data registered in database");
+        console.log(this);
+
+        return res.render("create-point.html", { saved: true });
+    });
+});
+
+server.get("/search-results", (req, res) => {
+    const search = req.query.search;
+
+    if (search == "") {
+        // empity search
+        return res.render("search-results.html", { total: 0 });
+    }
+
+    // get the datas of database
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
+        if (err) {
+            console.log(err);
+        }
+
+        // shows the HTML page with the datas of database
+        return res.render("search-results.html", {
+            places: rows,
+            total: rows.length,
+        });
+    });
+});
+
+// turn on the server
+server.listen(3000);
